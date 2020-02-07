0773584aa8b379778ec4a13ca5c01b13b799f76a
Robert DiNicolas <bob@dinicolas.com>
2020-02-06 22:45:27 -0500

    swapped FTP libraries, needs to support regular FTP

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 'use strict';
 
 const fs = require('file-system');
+const ftp = require("basic-ftp");
 const readline = require('readline');
-const Client = require('ssh2-sftp-client');
 const config = JSON.parse(fs.readFileSync(`${process.cwd()}/fhm_sync_config.json`).toString());
 const secret = require('./secret.json');
 const SimpleCrypto = require('simple-crypto-js').default;
 const simpleCrypto = new SimpleCrypto(secret.value);
-const client = new Client();
 const encrypted = config.encrypted;
-const username = encrypted ? simpleCrypto.decrypt(config.username) : config.username;
+const user = encrypted ? simpleCrypto.decrypt(config.username) : config.username;
 const password = encrypted ? simpleCrypto.decrypt(config.password) : config.password;
 const host = encrypted ? simpleCrypto.decrypt(config.host) : config.host;
 
-
-async function start() {
-
-  async function getFiles() {
+ 
+getFiles();
+ 
+async function getFiles() {
+    const client = new ftp.Client();
+    // client.ftp.verbose = true;
 
     try {
-      await client.connect({ username, password, host });
-      client.on('download', info => {
-        console.log(`-- download ${info.source}`);
-      });
-      return await client.downloadDir(config.source, config.dest);
-    } finally {
-      client.end();
+        await client.access({ host, user, password, secure: false });
+
+        client.trackProgress(info => {
+          console.log("File", `${info.name} ${info.bytes}/${info.bytesOverall}`);
+        })
+
+        await client.downloadToDir(config.dest, config.source);
     }
-  }
-    
-  await getFiles()
-    .then(msg => { console.log(`-----------\n-- result   ${msg}`) })
-    .catch(err => { console.log(`error: ${err.message}`) })
+    catch(err) {
+      console.log(err)
+    }
+    finally {
+      readline.createInterface(process.stdin, process.stdout)
+        .question("\nFinished.  Press [Enter] to exit...", () => {
+          process.exit();
+        });
+    }
 
-  readline.createInterface(process.stdin, process.stdout)
-    .question("\nPress [Enter] to exit...", () => {
-      process.exit();
-  });
-}
-
-start();
+    client.close()
+};
